8248e4538aab32641d2095b5a15486b3db3de13f
Kevin Cordero <ismael-3310@hotmail.com>
2021-11-17 18:02:19 -0500

    HttpClient for the api

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {login} from '../modelo/login';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,9 +9,11 @@ import {login} from '../modelo/login';
 })
 export class HomePage {
   login: login = new login();
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
   logearse() {
     console.log(this.login);
+    const res = this.http.get('https://jsonplaceholder.typicode.com/todos/1');
+    res.subscribe((data)=>console.log(data));
   }
 }
